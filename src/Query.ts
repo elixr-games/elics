2a266dd8b2de2fb29aa6a64d26e178dcef56bd59
Tianrui Zhang <felixtrz96@gmail.com>
2023-12-09 10:45:43 -0800

    Add queryId to Query

--- src/Query.ts
+++ src/Query.ts
@@ -1,30 +1,84 @@
 import { Component, ComponentMask } from './Component';
 
+export const PRIVATE = Symbol('@elics/query');
+
 export class Query {
-	private requiredComponents: Set<ComponentMask>;
-	private excludedComponents: Set<ComponentMask>;
+	[PRIVATE]: {
+		requiredComponents: Set<ComponentMask>;
+		excludedComponents: Set<ComponentMask>;
+		queryId: string;
+	} = {
+		requiredComponents: new Set(),
+		excludedComponents: new Set(),
+		queryId: '',
+	};
 
 	constructor(
 		required: (typeof Component)[],
 		excluded: (typeof Component)[] = [],
 	) {
-		this.requiredComponents = new Set(required.map((c) => c.bitmask || 0));
-		this.excludedComponents = new Set(excluded.map((c) => c.bitmask || 0));
+		this[PRIVATE].requiredComponents = new Set(
+			required.map((c) => c.bitmask || 0),
+		);
+		this[PRIVATE].excludedComponents = new Set(
+			excluded.map((c) => c.bitmask || 0),
+		);
+
+		this[PRIVATE].queryId = Query.generateQueryId(
+			this[PRIVATE].requiredComponents,
+			this[PRIVATE].excludedComponents,
+		);
+	}
+
+	static generateQueryId(
+		requiredComponents: Set<ComponentMask>,
+		excludedComponents: Set<ComponentMask>,
+	): string {
+		const requiredMask = Array.from(requiredComponents).reduce(
+			(acc, val) => acc | val,
+			0,
+		);
+		const excludedMask = Array.from(excludedComponents).reduce(
+			(acc, val) => acc | val,
+			0,
+		);
+		return `required:${requiredMask}|excluded:${excludedMask}`;
 	}
 
 	matchesMask(mask: ComponentMask): boolean {
-		for (let requiredMask of this.requiredComponents) {
+		for (let requiredMask of this[PRIVATE].requiredComponents) {
 			if ((mask & requiredMask) !== requiredMask) {
 				return false;
 			}
 		}
 
-		for (let excludedMask of this.excludedComponents) {
+		for (let excludedMask of this[PRIVATE].excludedComponents) {
 			if ((mask & excludedMask) === excludedMask) {
 				return false;
 			}
 		}
 
 		return true;
+	}
+
+	static matchesQuery(queryId: string, mask: ComponentMask): boolean {
+		const [requiredPart, excludedPart] = queryId.split('|');
+		const requiredMask = parseInt(requiredPart.split(':')[1]);
+		const excludedMask = parseInt(excludedPart.split(':')[1]);
+
+		if ((mask & requiredMask) !== requiredMask) {
+			return false;
+		}
+
+		// Skip the excluded mask check if excludedMask is 0 (no components to exclude)
+		if (excludedMask !== 0 && (mask & excludedMask) === excludedMask) {
+			return false;
+		}
+
+		return true;
+	}
+
+	get queryId(): string {
+		return this[PRIVATE].queryId;
 	}
 }
--- tests/Query.test.ts
+++ tests/Query.test.ts
@@ -41,5 +41,62 @@ describe('Query', () => {
 		expect(queryWithExclusion.matchesMask(combinedMask)).toBeFalsy();
 	});
 
-	// Additional tests for different combinations and edge cases
+	test('should correctly generate query identifiers', () => {
+		const queryIdWithMock = queryWithMock.queryId;
+		const queryIdWithBoth = queryWithBoth.queryId;
+		const queryIdWithExclusion = queryWithExclusion.queryId;
+
+		// Test if query IDs are unique and correctly formatted
+		expect(queryIdWithMock).not.toBe(queryIdWithBoth);
+		expect(queryIdWithExclusion).not.toBe(queryIdWithMock);
+		expect(queryIdWithExclusion).not.toBe(queryIdWithBoth);
+
+		// Check the format of the generated queryId
+		expect(queryIdWithMock).toMatch(/required:\d+\|excluded:\d+/);
+		expect(queryIdWithBoth).toMatch(/required:\d+\|excluded:\d+/);
+		expect(queryIdWithExclusion).toMatch(/required:\d+\|excluded:\d+/);
+	});
+
+	test('direct test of matchesQuery', () => {
+		const directMatch = Query.matchesQuery('required:1|excluded:0', 1);
+		expect(directMatch).toBeTruthy();
+	});
+
+	test('should correctly match query identifiers with component masks', () => {
+		const mockMask = MockComponent.bitmask;
+		const anotherMask = AnotherComponent.bitmask;
+		const combinedMask = mockMask | anotherMask;
+
+		// Test the static method matchesQuery
+		expect(Query.matchesQuery(queryWithMock.queryId, mockMask)).toBeTruthy();
+		expect(
+			Query.matchesQuery(queryWithMock.queryId, combinedMask),
+		).toBeTruthy();
+		expect(
+			Query.matchesQuery(queryWithBoth.queryId, combinedMask),
+		).toBeTruthy();
+		expect(Query.matchesQuery(queryWithBoth.queryId, mockMask)).toBeFalsy();
+		expect(
+			Query.matchesQuery(queryWithExclusion.queryId, mockMask),
+		).toBeTruthy();
+		expect(
+			Query.matchesQuery(queryWithExclusion.queryId, combinedMask),
+		).toBeFalsy();
+	});
+
+	test('matchesQuery should handle both zero and non-zero excluded masks', () => {
+		const mockMask = MockComponent.bitmask;
+		const anotherMask = AnotherComponent.bitmask;
+
+		// Query with no excluded components (excluded mask is 0)
+		const queryNoExclude = new Query([MockComponent]);
+		expect(Query.matchesQuery(queryNoExclude.queryId, mockMask)).toBeTruthy();
+
+		// Query with an excluded component (excluded mask is not 0)
+		const queryWithExclude = new Query([MockComponent], [AnotherComponent]);
+		expect(Query.matchesQuery(queryWithExclude.queryId, mockMask)).toBeTruthy();
+		expect(
+			Query.matchesQuery(queryWithExclude.queryId, anotherMask),
+		).toBeFalsy(); // Should fail as AnotherComponent is excluded
+	});
 });
