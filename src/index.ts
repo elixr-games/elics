4f105389c167f4373c6f8c13915355296d3dc73e
Felix Zhang <felix@elixr.games>
2025-03-26 14:46:04 -0700

    export everything

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
-export { Entity } from './Entity.js';
-export { World } from './World.js';
-export { Query, QueryConfig } from './Query.js';
-export { createComponent } from './Component.js';
-export { createSystem } from './System.js';
-export { Types } from './Types.js';
-export { VERSION } from './Version.js';
+export * from './Entity.js';
+export * from './World.js';
+export * from './Query.js';
+export * from './Component.js';
+export * from './System.js';
+export * from './Types.js';
+export * from './Version.js';
